51d24d89804b75753c642b2d9691b53ada06a9e6
嶋田壮英 <[kumamon0136w@gmail.com]>
2022-06-27 21:18:04 +0900

    javaコンテンツ基礎３章まで

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,6 +1,13 @@
 
 public class Chapter02 {
 	public static void main(String[] args) {
-	    System.out.println("こんにちは！");
-    }
+		System.out.println("Hello WEBCANP!");
+		System.out.println("Hello Java!");
+	}
 }
+//1行コメントテスト
+
+/*
+複数行コメント
+テスト
+*/
